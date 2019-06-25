2f6158f99b785eeeb631d1be0d60b7a43a9e2b52
BC30138 <sasori.axele@gmail.com>
2019-06-25 18:36:32 +0300

    new system

--- toPDF/pandoc_md_report.sh
+++ toPDF/pandoc_md_report.sh
@@ -2,4 +2,4 @@
 
 filename=$(echo "$1" | cut -f 1 -d '.')
 
-pandoc  $1 --listings --template=templates/template.tex -V lang=eu --pdf-engine=xelatex -o "$filename.pdf"
+pandoc  $1 --listings --template=templates/template.tex -V lang=eu --pdf-engine=xelatex -o "$filename.pdf" 
